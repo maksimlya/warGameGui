30408fff0f4d7dbdc06198a01044f242fbdfd3ac
Maksim Lyashenko <maksimlya@gmail.com>
2018-08-26 16:51:09 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,158 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ReactDOM from 'react-dom';
+import TextField from '@material-ui/core/TextField';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
+
+const styles = theme => ({
+    textField: {
+        marginRight: 20,
+        width: 150,
+    },
+    menu: {
+        width: 100,
+    },
+    button: {
+        width: 150,
+    },
+});
+
+const hideOptions = [
+    {
+        value: 'true',
+        label: 'True',
+    },
+    {
+        value: 'false',
+        label: 'False',
+    },
+];
+const destructorOptions = [
+    {
+        value: 'ship',
+        label: 'Ship',
+    },
+    {
+        value: 'plane',
+        label: 'Plane',
+    },
+];
+
 class App extends Component {
-  render() {
+    state = {
+        launcherId: 'L101',
+        destructorId: 'D201',
+        isHidden: 'True',
+        type: 'Ship'
+    };
+    handleChange = id => event => {
+        this.setState({
+            [id]: event.target.value,
+        });
+    };
+
+
+
+    render() {
+    const { classes } = this.props;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Afeka-War-Game Console</h1>
         </header>
-        <p className="App-intro">
-            <Button variant="contained" color="primary">
-                Hello World
-            </Button>
-        </p>
+
+            <form className={classes.container} method="post" action="http://localhost:4567/addMissileLauncher" target="hiddenFrame" noValidate autoComplete="off">
+                <TextField
+                    name="id"
+                    id="name"
+                    label="Launcher ID"
+                    className={classes.textField}
+                    value={this.state.launcherId}
+                    onChange={this.handleChange('launcherId')}
+                    margin="normal"
+                />
+
+                <TextField
+                    name="isHidden"
+                    id="isHidden"
+                    select
+                    label="Is Hidden?"
+                    className={classes.textField}
+                    value={this.state.isHidden}
+                    onChange={this.handleChange('isHidden')}
+                    SelectProps={{
+                        native: true,
+                        MenuProps: {
+                            className: classes.menu,
+                        },
+                    }}
+                    margin="normal"
+                >
+                    {hideOptions.map(option => (
+                       <option key={option.value} value={option.value}>
+                            {option.label}
+                        </option>
+                    ))}
+                </TextField>
+                <Button
+                    variant="contained"
+                    type="submit"
+                    color="primary"
+                    className={classes.button}>
+                    Add Missile Launcher
+                </Button>
+            </form>
+            <form className={classes.container} method="post" action="http://localhost:4567/addLauncherDestructor" target="hiddenFrame" noValidate autoComplete="off">
+                  <TextField
+                      name="id"
+                      id="name"
+                      label="Destructor ID"
+                      className={classes.textField}
+                      value={this.state.destructorId}
+                      onChange={this.handleChange('destructorId')}
+                      margin="normal"
+                  />
+
+                  <TextField
+                      name="type"
+                      id="type"
+                      select
+                      label="Destructor type"
+                      className={classes.textField}
+                      value={this.state.type}
+                      onChange={this.handleChange('type')}
+                      SelectProps={{
+                          native: true,
+                          MenuProps: {
+                              className: classes.menu,
+                          },
+                      }}
+                      margin="normal"
+                  >
+                      {destructorOptions.map(option => (
+                          <option key={option.value} value={option.value}>
+                              {option.label}
+                          </option>
+                      ))}
+                  </TextField>
+                  <Button variant="contained" type="submit" color="primary"  className={classes.button}>
+                      Add Launcher Destructor
+                  </Button>
+              </form>
+          <iframe name="hiddenFrame" className="hide">a</iframe>
       </div>
+
     );
   }
 }
 
-export default App;
+App.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+//export default App;
+export default withStyles(styles)(App);
